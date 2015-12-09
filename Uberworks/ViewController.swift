ce681469911ef44fc104037508d607ca4fc23e45
Pablo Alcalde <pablo.alcalde@rentalia.com>
2015-12-09 18:22:30 +0100

    first implementation done.

--- Uberworks/ViewController.swift
+++ Uberworks/ViewController.swift
@@ -19,15 +19,54 @@ func delay(seconds seconds: Double, completion:()->()) {
 
 class ViewController: UIViewController {
     let duration = 1.0
-    let buttonLayer = CAShapeLayer()
-    
+//    var image = UIImage(named: "like")
+    var image = UIImage(named: "gps-marker")
+
     override func viewDidLoad() {
         super.viewDidLoad()
         triggerAnimation()
+        self.view.userInteractionEnabled = false
+    }
+    
+    private func createImageLayers(image: UIImage) -> CALayer {
+        let imageFrame = CGRectMake(0, 0, 20, 20)
+        let imgCenterPoint = CGPointMake(CGRectGetMidX(imageFrame), CGRectGetMidY(imageFrame))
+        
+        let imageShapeLayer = CALayer()
+        imageShapeLayer.bounds = imageFrame
+        imageShapeLayer.position = self.view.center
+        imageShapeLayer.backgroundColor = UIColor.blackColor().CGColor
+        self.view.layer.addSublayer(imageShapeLayer)
+        
+        imageShapeLayer.mask = CALayer()
+        imageShapeLayer.mask!.contents = image.CGImage
+        imageShapeLayer.mask!.bounds = imageFrame
+        imageShapeLayer.mask!.position = imgCenterPoint
+        imageShapeLayer.contentsScale = UIScreen.mainScreen().scale
+        
+        let circleMaskTransform = CAKeyframeAnimation(keyPath: "transform")
+        circleMaskTransform.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut),CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut),CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)]
+
+        circleMaskTransform.duration = duration * 2
+        circleMaskTransform.values = [
+            NSValue(CATransform3D: CATransform3DIdentity),
+            NSValue(CATransform3D: CATransform3DMakeScale(1.3, 1.3, 1.0)),
+            NSValue(CATransform3D: CATransform3DIdentity)
+        ]
+        circleMaskTransform.keyTimes = [
+            0.0,
+            0.6,
+            1.0
+        ]
+
+        
+        imageShapeLayer.addAnimation(circleMaskTransform, forKey: circleMaskTransform.keyPath)
+        return imageShapeLayer
     }
     
     func triggerAnimation() {
-        self.view.layer.addSublayer(self.animatedCircleAtPoint(CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height/2), radius: 20))
+        self.view.layer.addSublayer(self.animatedCircleAtPoint(self.view.center, radius: 20))
+        self.view.layer.addSublayer(self.createImageLayers(self.image!))
     }
     
     func animatedCircleAtPoint(center: CGPoint, radius: CGFloat) -> CAShapeLayer {
@@ -40,6 +79,7 @@ class ViewController: UIViewController {
         circle.lineCap = kCALineCapRound
         circle.strokeColor = UIColor.whiteColor().CGColor
         circle.fillColor = UIColor.whiteColor().CGColor
+        circle.contents = image?.CGImage
         
         let animation = CABasicAnimation(keyPath: "path")
         animation.fromValue = circlePath.CGPath
@@ -52,12 +92,12 @@ class ViewController: UIViewController {
         animation.delegate = self
         circle.addAnimation(animation, forKey: animation.keyPath)
         
+        
         delay(seconds: duration) { () -> () in
             circle.addSublayer((self.animatedCircleBorderAtPoint(center, radius: expandedRadius)))
         }
         
-        buttonLayer.addSublayer(circle)
-        return buttonLayer
+        return circle
     }
     
     func animatedCircleBorderAtPoint(center: CGPoint, radius: CGFloat) -> CAShapeLayer {
@@ -69,6 +109,7 @@ class ViewController: UIViewController {
         circleBorder.lineCap = kCALineCapRound
         circleBorder.strokeColor = UIColor.whiteColor().CGColor
         circleBorder.fillColor = UIColor.clearColor().CGColor
+        circleBorder.lineWidth = 2.0
         circleBorder.opacity = 1
         
         let animation = CABasicAnimation(keyPath: "path")
@@ -88,9 +129,8 @@ class ViewController: UIViewController {
     }
     
     override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
-        for layer in self.buttonLayer.sublayers! {
-            layer.removeFromSuperlayer()
-        }
+        print(self.view.layer.sublayers)
+        self.view.layer.sublayers = nil;
         triggerAnimation()
     }
 }
